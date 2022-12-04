cec1ee716c50ae536e92b5acf424da76b36bb2ed
Zphorium <106176680+Zphorium@users.noreply.github.com>
2022-12-04 19:48:15 +0400

    Validations uploaded

---
+++ DemoApp/DemoApplication/Validators/Admin/Navbar/Add/AddViewModelValidator.cs
@@ -0,0 +1,21 @@
+﻿using DemoApplication.ViewModels.Admin.Navbar;
+using FluentValidation;
+
+namespace DemoApplication.Validators.Admin.Navbar.Add
+{
+    public class AddViewModelValidator : AbstractValidator<AddViewModel>
+    {
+        public AddViewModelValidator()
+        {
+            RuleFor(avm => avm.Name)
+                .NotNull()
+                .WithMessage("Title can't be empty")
+                .NotEmpty()
+                .WithMessage("Title can't be empty")
+                .MinimumLength(10)
+                .WithMessage("Minimum length should be 10")
+                .MaximumLength(45)
+                .WithMessage("Maximum length should be 45");
+        }
+    }
+}
